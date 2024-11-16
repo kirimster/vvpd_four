13dc67bd975586d9a923c593399c28a7b3f37715
Alexander <alexanderbatyuta@yandex.ru>
2024-11-16 18:22:25 +0700

    Создание функции для выведения меню пункта 4, написаны заготовоки функций под 1-3 пункт меню

--- task.py
+++ task.py
@@ -10,6 +10,49 @@ def display_menu():
     )
 
 
+def display_func_menu():
+    """Показ функций для работы с матрицами"""
+    print("\nКакую функцию хотите выполнить?:\n"
+        "1. Замена всех элементов, которые меньше 10, числом 42\n"
+        "2. Печть индексов отрицательных элементов массива\n"
+        "3. Вычисление суммы элементов каждого столбца и строки\n"
+        "4. Вывести левую и правую диагональ двумерного массива\n"
+        "5. Нахождение столбца с наибольшой суммой элементов\n"
+        "6. Выход в меню\n")
+
+def display_create_massive():
+    print("\nКак вы хотите заполнить массив?:\n"
+          "1. Ввести значения вручную\n"
+          "2. Заполнить числами наугад\n"
+          "3. Выход в меню\n"
+          )
+
+
+def size_matrix():
+    """Ввод размера массива"""
+    print("\nВвод размера матрицы\n")
+    try:
+        m=int(input("Сколько будет строчек?: "))
+        if m<0:
+            raise ValueError("Число должно быть положительным!!!")
+        n=int(input("\nСколько будет столбцов?: "))
+        if n<0:
+            raise ValueError("Число должно быть положительным!!!")
+    except ValueError as err:
+        print(err.args)
+        print("Ошибка: Неверный ввод. Пожалуйста, введите корректные данные.")
+
+
+def create_massive():
+    """Создание массива"""
+    while True:
+        display_create_massive()
+        menu_create_variable = input("Выберите опцию: ")
+        match menu_create_variable:
+            case "3":
+                break
+
+
 def main():
     """Основная функция"""
     while True:
@@ -25,8 +68,7 @@ def main():
                     display_func_menu()
                     menu_func_variable = input("Выберите опцию: ")
                     match menu_func_variable:
-                        case "1":
-                            replace_numbers()
+
                         case "6":
                             break
                         case _:
